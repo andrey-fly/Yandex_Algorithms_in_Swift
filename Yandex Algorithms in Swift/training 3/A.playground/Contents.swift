10beb9f2b70540d475277ef8bf47e4ff0d084693
Mukhin Andrey <mukhin.av.1997@gmail.com>
2021-11-02 13:08:37 +0300

    Add solution for 3A

--- Yandex Algorithms in Swift/training 3/A.playground/Contents.swift
+++ Yandex Algorithms in Swift/training 3/A.playground/Contents.swift
@@ -1,1 +1,9 @@
+import Foundation
 
+if let path = Bundle.main.path(forResource: "input", ofType: "txt") {
+    freopen(path, "r", stdin)
+}
+
+let numbersArray = readLine()!.split(separator: " ")
+let numbersSet = Set(numbersArray)
+print(numbersSet.count)
