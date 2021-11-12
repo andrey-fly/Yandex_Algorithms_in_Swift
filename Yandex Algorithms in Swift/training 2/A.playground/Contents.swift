69d972b12eef6c63305653467d669d6094e1e277
Mukhin Andrey <mukhin.av.1997@gmail.com>
2021-11-12 18:02:30 +0300

    Add solution for 2A

--- Yandex Algorithms in Swift/training 2/A.playground/Contents.swift
+++ Yandex Algorithms in Swift/training 2/A.playground/Contents.swift
@@ -1,1 +1,18 @@
+import Foundation
 
+if let path = Bundle.main.path(forResource: "input", ofType: "txt") {
+    freopen(path, "r", stdin)
+}
+
+func isGrowth(array: [Int]) -> Bool {
+    
+    for i in 1..<array.count {
+        if array[i-1] >= array[i] {
+            return false
+        }
+    }
+    return true
+}
+
+let array = readLine()!.split(separator: " ").map { Int($0)! }
+print(isGrowth(array: array) ? "YES" : "NO")
