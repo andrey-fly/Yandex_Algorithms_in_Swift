bb83c69100dde7023520ae17d07e03a7e0e73588
Mukhin Andrey <mukhin.av.1997@gmail.com>
2021-12-04 21:29:55 +0300

    Add solution for 6A

--- Yandex Algorithms in Swift/training 6/A.playground/Contents.swift
+++ Yandex Algorithms in Swift/training 6/A.playground/Contents.swift
@@ -1,1 +1,30 @@
+import Foundation
+
+if let path = Bundle.main.path(forResource: "input", ofType: "txt") {
+    freopen(path, "r", stdin)
+}
+
+let _ = readLine()
+let arrayFirst = readLine()!.split(separator: " ").map { Int($0)! }
+let arraySecond = readLine()!.split(separator: " ").map { Int($0)! }
+
+func binarySearch(array a: [Int], value v: Int) -> Bool {
+    var leftBound = 0
+    var rightBound = a.count
+    while leftBound < rightBound {
+        let midIndex = (leftBound + rightBound) / 2
+        if a[midIndex] == v {
+            return true
+        } else if a[midIndex] < v {
+            leftBound = midIndex + 1
+        } else {
+            rightBound = midIndex
+        }
+    }
+    return false
+}
+
+for item in arraySecond {
+    print(binarySearch(array: arrayFirst, value: item) ? "YES" : "NO")
+}
 
