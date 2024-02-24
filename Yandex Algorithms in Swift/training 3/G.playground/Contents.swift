97dfc551d0248ccd62e22de64c9ba458e1796ebe
Andrey Mukhin <Andrey_Mukhin@russianpost.ru>
2024-02-24 17:16:49 +0300

    Add solution for 3G, 3H

--- Yandex Algorithms in Swift/training 3/G.playground/Contents.swift
+++ Yandex Algorithms in Swift/training 3/G.playground/Contents.swift
@@ -1,3 +1,23 @@
-import Cocoa
+import Foundation
 
-var greeting = "Hello, playground"
+if let path = Bundle.main.path(forResource: "input", ofType: "txt") {
+    freopen(path, "r", stdin)
+}
+
+let amount = Int(readLine()!)!
+var turtleSay: [Int]
+
+var beenSet: Set<[Int]> = .init()
+var answer = 0
+
+for _ in 0..<amount {
+    turtleSay = readLine()!.split(separator: " ").map { Int($0)! }
+    let isNonNegative = turtleSay[0] >= 0 && turtleSay[1] >= 0
+    let isEqualAmountOfTurtles = turtleSay[0] + turtleSay[1] == amount - 1
+    if isNonNegative && isEqualAmountOfTurtles && !beenSet.contains(turtleSay) {
+        answer += 1
+        beenSet.insert(turtleSay)
+    }
+}
+
+print(answer)
--- Yandex Algorithms in Swift/training 3/H.playground/Contents.swift
+++ Yandex Algorithms in Swift/training 3/H.playground/Contents.swift
@@ -1,3 +1,14 @@
-import Cocoa
+import Foundation
 
-var greeting = "Hello, playground"
+if let path = Bundle.main.path(forResource: "input", ofType: "txt") {
+    freopen(path, "r", stdin)
+}
+
+let amount = Int(readLine()!)!
+var xSet: Set<Substring> = .init()
+
+for _ in 0..<amount {
+    xSet.insert(readLine()!.split(separator: " ")[0])
+}
+
+print(xSet.count)
